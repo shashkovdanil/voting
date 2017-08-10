1ea841a4e6c6ff70d2e294a17480b8d15a0074a6
Danil Shashkov <shashkovdanil@gmail.com>
2017-08-10 22:03:04 +0500

    init

---
+++ src/core.js
@@ -0,0 +1,17 @@
+import { List, Map } from 'immutable'
+
+export const setEntries = (state, entries) => state.set('entries', List(entries))
+
+export const next = (state) => {
+  const entries = state.get('entries')
+  return state.merge({
+    vote: Map({ pair: entries.take(2) }),
+    entries: entries.skip(2)
+  })
+}
+
+export const vote = (state, entry) => state.updateIn(
+  ['vote', 'tally', entry],
+  0,
+  tally => tally + 1
+)
